25ae1059ca8d108436b3d9f6da6d4cca42e68fcd
Deliman206 <smiller206@me.com>
2019-01-04 11:54:36 -0800

    cleaned up & md

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Prework-CodeChallenges
 CF 401d6 prework coding challenges 
+
+### Problem 1: Array Max Result
+
+#Problem Statement
+Given an array select a number that exists, and output the computated "score". The method you create should take in both an array of integers and the integer the user selected.
+
+Create a Console application that requests 5 numbers between 1-10. Output the array to the console and Ask the user to select a number. After the selection, output the "score" of the number chosen.
+
+#Output Image
+![cr1](ArrayMultiplier.png)
--- prework-code-challenges/prework-code-challenges/Program.cs
+++ prework-code-challenges/prework-code-challenges/Program.cs
@@ -14,27 +14,26 @@ namespace prework_code_challenges
         static void Main(string[] args)
         {
             string userName;
-            int chosenNum;
+            int num1 = 0, num2 = 0, num3 = 0, num4 = 0, num5 = 0, chosenNum;
+            int[] numArray = new int[5] { num1, num2, num3, num4, num5 };
             int multiplier = 0;
+            string[] questions = new string[5] { "Your First Number...", "Your Second Number...", "Your Third Number...", "Your Fourth Number...", "Your Fifth Number..." };
             Console.WriteLine("Hello! This is interactive game, so what's your name?");
             userName = Console.ReadLine();
             Console.WriteLine("Well {0} I'm pleased to meet you,\nI am the computer. Salutations!", userName);
             Console.WriteLine("Let's get started shall we. Please enter 5 numbers between 1-10.\nI will prompt you for each one individually.");
-            Console.WriteLine("Your First Number...");
-            Int32.TryParse(Console.ReadLine(), out int num1);
-            if (num1 > 10 || num1 < 1)
+            for (int i = 0; i < questions.Length; i++)
             {
-                Console.WriteLine("oops you fucked up");
+                Console.WriteLine(questions[i]);
+                Int32.TryParse(Console.ReadLine(), out int num);
+                if (num > 10 || num < 1)
+                {
+                    Console.WriteLine("That Number is not between 1-10, please try again.");
+                    i--;
+                }
+                numArray[i] = num;
             }
-            Console.WriteLine("Your Second Number...");
-            Int32.TryParse(Console.ReadLine(), out int num2);
-            Console.WriteLine("Your Third Number...");
-            Int32.TryParse(Console.ReadLine(), out int num3);
-            Console.WriteLine("Your Fourth Number...");
-            Int32.TryParse(Console.ReadLine(), out int num4);
-            Console.WriteLine("Your Fifth Number...");
-            Int32.TryParse(Console.ReadLine(), out int num5);
-            int[] numArray = new int[5] { num1, num2, num3, num4, num5 };
+
             Console.WriteLine("Fantastic! Now choose any one of those Numbers and I will tell you your Score.");
             Int32.TryParse(Console.ReadLine(), out chosenNum);
 
